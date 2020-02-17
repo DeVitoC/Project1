b738f7436090b07fc77e5f3a681d183a92392d5c
Christopher DeVito <toolofjesus@protonmail.com>
2020-02-17 00:47:45 -0500

    build view controller and detail view in storyboard and added detailviewcontroller.swift

---
+++ Project1/View Controllers/DetailViewController.swift
@@ -0,0 +1,34 @@
+//
+//  DetailViewController.swift
+//  Project1
+//
+//  Created by Lambda_School_Loaner_259 on 2/17/20.
+//  Copyright © 2020 DeVitoC. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    // MARK: IBOutlets
+    @IBOutlet var imageView: UIImageView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
